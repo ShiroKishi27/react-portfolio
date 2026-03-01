0e2d08bca4e455a5be7709ed62f412a55d5c9b53
ShiroKishi27 <heimslycalauagan@gmail.com>
2026-03-01 13:46:34 +0800

    add featured project

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -8,12 +8,37 @@ function Project() {
     >
       <AnimateOnScroll>
         <div className="mx-auto max-w-5xl px-6">
-          <h2 className="mb-8 bg-gradient-to-r from-blue-800 to-cyan-300 bg-clip-text text-center text-3xl font-bold text-transparent">
+          <h2 className="mb-8 bg-linear-to-r from-blue-800 to-cyan-300 bg-clip-text text-center text-3xl font-bold text-transparent">
             Featured Project
           </h2>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
             <div className="rounded-xl border border-white/10 bg-white/2 p-6 text-white shadow-[0_8px_32px_rgba(0,0,0,0.15)] backdrop-blur-sm hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
-              <div className="absolute inset-0 -z-1 rounded-xl bg-gradient-to-tl from-white/30 to-transparent opacity-30"></div>
+              <div className="absolute inset-0 -z-1 rounded-xl bg-linear-to-tl from-white/30 to-transparent opacity-30"></div>
+              <div className="absolute inset-0 -z-1 rounded-xl ring-1 ring-white/30"></div>
+
+              <h3 className="mb-2 text-xl">
+                <strong>Classic Sudoku App</strong>
+              </h3>
+              <p className="mb-2 text-gray-400">
+                A modern, interactive Sudoku web application built with React
+                that allows users to play dynamically generated puzzles fetched
+                from an external API.
+              </p>
+              <div className="flex flex-wrap gap-1">
+                {["React JS", "Vite", "Tailwind CSS", "Lucide-React"].map(
+                  (items, key) => (
+                    <span
+                      key={key}
+                      className="rounded-full bg-blue-500/10 px-3 py-1 text-sm text-blue-500 transition hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]"
+                    >
+                      {items}
+                    </span>
+                  ),
+                )}
+              </div>
+            </div>
+            <div className="rounded-xl border border-white/10 bg-white/2 p-6 text-white shadow-[0_8px_32px_rgba(0,0,0,0.15)] backdrop-blur-sm hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
+              <div className="absolute inset-0 -z-1 rounded-xl bg-linear-to-tl from-white/30 to-transparent opacity-30"></div>
               <div className="absolute inset-0 -z-1 rounded-xl ring-1 ring-white/30"></div>
 
               <h3 className="mb-2 text-xl">
@@ -37,7 +62,7 @@ function Project() {
               </div>
             </div>
             <div className="rounded-xl border border-white/10 bg-white/2 p-6 text-white shadow-[0_8px_32px_rgba(0,0,0,0.15)] backdrop-blur-sm hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
-              <div className="absolute inset-0 -z-1 rounded-xl bg-gradient-to-tl from-white/30 to-transparent opacity-30"></div>
+              <div className="absolute inset-0 -z-1 rounded-xl bg-linear-to-tl from-white/30 to-transparent opacity-30"></div>
               <div className="absolute inset-0 -z-1 rounded-xl ring-1 ring-white/30"></div>
 
               <h3 className="mb-2 text-xl">
@@ -61,7 +86,7 @@ function Project() {
               </div>
             </div>
             <div className="rounded-xl border border-white/10 bg-white/2 p-6 text-white shadow-[0_8px_32px_rgba(0,0,0,0.15)] backdrop-blur-sm hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
-              <div className="absolute inset-0 -z-1 rounded-xl bg-gradient-to-tl from-white/30 to-transparent opacity-30"></div>
+              <div className="absolute inset-0 -z-1 rounded-xl bg-linear-to-tl from-white/30 to-transparent opacity-30"></div>
               <div className="absolute inset-0 -z-1 rounded-xl ring-1 ring-white/30"></div>
 
               <h3 className="mb-2 text-xl">
